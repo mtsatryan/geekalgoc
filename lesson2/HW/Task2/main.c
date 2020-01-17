f4a68773dfc0b69540a317541499fa33dc41bb47
mgts <mtsatryan@yandex.ru>
2020-01-17 20:03:10 +0300

    UPD
    Done Task2 lesson 2

--- lesson2/HW/Task2/main.c
+++ lesson2/HW/Task2/main.c
@@ -1,6 +1,44 @@
+// powering integer a on power of b
+
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+void iter_power(int a, int b){
+    int	result;
+
+    result = 1;
+    if (b < 0)
+        printf("Is Negative");
+    if (b == 0)
+        printf("Is OK");
+    while (b)
+    {
+        result *= a;
+        b--;
+    }
+    printf("%d", result);
+}
+
+int rec_power(int a, int b){
+
+    if (b < 0)
+        return(0);
+    if (b == 0)
+        return(1);
+     return (a * (rec_power(a, b - 1)));
+
+}
+
+int main(int argc, char **argv) {
+    int a;
+    int b;
+    printf("Print your number: ");
+    scanf("%d", &a);
+
+    printf("Print the power of it: ");
+    scanf("%d", &b);
+
+    iter_power(a, b);
+    printf("\n");
+    printf("%d", rec_power(a, b));
     return 0;
 }
